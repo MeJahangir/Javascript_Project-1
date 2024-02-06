2e8c11c1987f19c2fb27520864e075b1269e78e5
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-02-07 01:46:24 +0600

    discount system add .

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-// conditions area add
+// conditions area discount mathord add
 let price = 20000;
+let discountPercentage;
+let discount;
 
-if (price => 20000) {
-    let discountPercentage = 10;
-    let discount = (discountPercentage * price)/100;
-    
+if (price >= 20000) {
+    discountPercentage = 10;
+    discount = (discountPercentage * price) / 100;
+    console.log(discount);
+} else if (price >= 10000){
+    discountPercentage = 5;
+    discount = (discountPercentage * price) / 100;
+    console.log(discount);
+}else{
+    console.log("No discount applicable!");
 }
