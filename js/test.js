fdd1b1576430564c6d25a79baad9e42ebff4e6c8
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-02-13 23:11:14 +0600

    conditon check and continue

---
+++ js/test.js
@@ -0,0 +1,19 @@
+// Test array value check
+
+// let names = ['jahangir', 'alam', 'jakariya', 'nur', 'mehedi', 'yeasin', 'hamid', 'mamun'];
+// let i = 0; 
+// let nameLength = names.length;
+
+// while (i < nameLength) {
+//     console.log(names[i]);
+
+//     // conditon check korbe, jodi same hoy tahole continue er pore loop check korbe na
+//     if (names[i] == 'yeasin') {
+//         i++;
+//         console.log('I am a good person');
+//         continue;
+//     }
+//     console.log('Yeasin Brother is a very rotten person');
+//     i++;
+// }
+
