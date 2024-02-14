542331bfb9911d10e4f6bf17c2ee09afcf0153db
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-02-14 23:05:39 +0600

    submit

--- js/test.js
+++ js/test.js
@@ -18,4 +18,26 @@
 // }
 
 var isSingle = true;
-console.log(isSingle);
+// console.log(isSingle);
+
+var price = 5;
+price -= 5;
+price /= 5;
+price *= 5;
+console.log(price);
+
+var myName = "jahangri";
+var lastName = "alam";
+var fullName = myName + " " + lastName;
+console.log(fullName);
+
+for (var i = 0; i < 100; i++)
+{
+    // check that the number is even => jor and odd => bijor
+    if (i % 2 == 0)
+    {
+         continue;
+    }
+    // if we got here, then i is odd.
+    console.log(i + " is an odd number.");
+}
