484f730d8f00d0c6da7066ab3bad12a787de1c37
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-02-18 01:26:06 +0600

    task

---
+++ js/basic_task_note.js
@@ -0,0 +1,60 @@
+/*
+............
+Variable
+............
+What is javascript ?
+How does js works?
+What is variable? *
+Declare a variable*
+Types of variable? How can you find out type of a variable*
+Primitive and non-primitive data types
+Naming Convention of js variables
+Math Operation +.-.*.%
+Short hand : +=. -=. *=. /=
+++, --
+parseInt, parseFloat
+toFixed
+*/
+
+/*
+............
+Array
+............
+What is the purpose array?
+how to declare an array in js.
+number of elements in an array.
+what is index?
+find the velue of an element by index.
+Change an element by index.
+get the index of an element by the value.
+what does it mean when you get undefined while getting the value of an elements.
+how can you add an element to an array at the last positon
+how can you remove an element from array at the first position
+add an element at the first positon of an array
+Remove the first element of an array
+*/
+
+/*
+...................
+javascript basic
+...................
+1. Meaning of : >, <, >=, <=, ==, !=, ===, !==;
+2. Meaning of &&
+3. Meaning of ||
+4. Let's say you have x amount of money. if you have more than 80000, then you will by apple laptop. if more then 40000 then you will by lenovo yoga. if you more then 20000 then you will by an used laptop. otherwise , you will use you mobile phone.
+*/
+/*
+................
+Loop
+................
+5.display "ajke amar mon valo nai" for 39 times
+6.Display numbers between 58 to 98
+7.Show all even numbers 582 to 624
+8. Show all odd numbers 411 to 623
+9.Difference between while loop and for loop
+10.Declare an array for all the topics that you have learned last few days display then as output
+11.Create an array for all the mobile phones. Display all the elements of the display then as output.
+12. Run a loop from 30 to 86. this loop will stop if the values get higher the 
+13.Write the price of the books that you have display the price is lower than 200.
+*/
+
---
+++ js/task.js
@@ -0,0 +1,50 @@
+// What is variable?
+// Ans: kono data k akta jayga te store kore rakhar jonno j sentax use kora hoy take variable bole
+// Example :
+var x = "md jahangir alam";
+let y = "md jahangir alam";
+const z = "md jahangir alam";
+
+
+var firstName = "jahangir";
+let lastName  = "alam";
+const fullName = "md jahangir alam";
+console.log(firstName);
+console.log(lastName);
+console.log(fullName);
+
+// result : jahangir
+// result : alam
+// result : md jahangir alam
+
+// Types of variable? How can you find out type of a variable
+var stringType = "md jahangir alam";
+var numberType = 100;
+var boolentype = true;
+var floatType = 3.22;
+
+console.log(typeof(stringType));
+console.log(typeof(numberType));
+console.log(typeof(boolentype));
+console.log(typeof(floatType));
+// result : (string, number, boolean, float) 
+
+// Primitive and non-primitive data types
+
+// Primitive Data Types 
+/*
+Number
+String
+Boolean
+Undefined
+Null
+Symbol
+*/
+
+// Non_Primitive Data Types 
+
+/*
+Object 
+Array
+*/
+
