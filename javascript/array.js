be5870f5553f8ead10c67d84c1e8677608c955b1
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-03-06 01:32:22 +0600

    folder change

--- javascript/array.js
+++ javascript/array.js
@@ -1,3 +1,4 @@
+
 // secend largest number print 
 
 const numbers = [30, 40, 4, 5, 30, 50, 45];
@@ -5,3 +6,11 @@ const numbers = [30, 40, 4, 5, 30, 50, 45];
 const sortedNumbers = numbers.sort((a, b) => a - b);
 const secondLargest = sortedNumbers[sortedNumbers.length - 2];
 console.log(secondLargest);
+
+// example :
+const sortedNum = numbers.splice((jahangir , alam) => jahangir - alam);
+const secondLarg = sortedNum[sortedNum.length - 2];
+console.log(secondLarg);
+
+// output : 45
+
