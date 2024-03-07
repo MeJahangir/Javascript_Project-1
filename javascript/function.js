091856f9c7a710f031aa3f6a68d5191afd1b318b
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-03-07 23:58:32 +0600

    condition

--- javascript/function.js
+++ javascript/function.js
@@ -1,6 +1,12 @@
-// javascript function add 
-function myFunction() {
-  if (condition) {
-    
+// Using a condition
+function isAdult(age) {
+  if (age >= 18) {
+      console.log("You are an adult.");
+  } else {
+      console.log("You are not an adult.");
   }
 }
+
+// Calling the function with different arguments
+isAdult(25); // Output: "You are an adult."
+isAdult(15);
