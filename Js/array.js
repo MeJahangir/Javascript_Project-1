d7317ba5c5e683999f443b171e10f754ee6bfd3c
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-03-18 23:15:48 +0600

    push

--- Js/array.js
+++ Js/array.js
@@ -1,66 +1,73 @@
 
-// secend largest number print 
+// // secend largest number print 
 
-const numbers = [30, 40, 4, 5, 30, 50, 45];
+// const numbers = [30, 40, 4, 5, 30, 50, 45];
 
-function getSecondLergest(arayOfNumbers){
-    let min = 0;
-    let max = 0;
+// function getSecondLergest(arayOfNumbers){
+//     let min = 0;
+//     let max = 0;
 
-    for (let index = 0; index < arayOfNumbers.length; index++) {
-        const element = arayOfNumbers[index];
-        if(element > min){
-            max = element; 
-        }
-        if (max = element) {
-            min.length - 1;
-        }
+//     for (let index = 0; index < arayOfNumbers.length; index++) {
+//         const element = arayOfNumbers[index];
+//         if(element > min){
+//             max = element; 
+//         }
+//         if (max = element) {
+//             min.length - 1;
+//         }
 
-        min = element;
+//         min = element;
        
-    }
+//     }
 
-    return max;
-}
+//     return max;
+// }
 
-let result = getSecondLergest(numbers);
-console.log(result);
+// let result = getSecondLergest(numbers);
+// console.log(result);
 
-console.log(sortedNumbers);
+// console.log(sortedNumbers);
 
-const secondLargest = sortedNumbers[sortedNumbers.length - 2];
-console.log(secondLargest);
+// const secondLargest = sortedNumbers[sortedNumbers.length - 2];
+// console.log(secondLargest);
 
-// example :
-const sortedNum = numbers.splice((jahangir , alam) => jahangir - alam);
-const secondLarg = sortedNum[sortedNum.length - 2];
-console.log(secondLarg);
-// example :
-const sort = numbers.sort((a, b) => a - b);
-const largest = sortedNumbers[sortedNumbers[1]];
-console.log(secondLargest);
+// // example :
+// const sortedNum = numbers.splice((jahangir , alam) => jahangir - alam);
+// const secondLarg = sortedNum[sortedNum.length - 2];
+// console.log(secondLarg);
+// // example :
+// const sort = numbers.sort((a, b) => a - b);
+// const largest = sortedNumbers[sortedNumbers[1]];
+// console.log(secondLargest);
 
-// // output : 45
+// // // output : 45
 
 
-const numbers = [30, 40, 4, 5, 30, 50, 45];
+// const numbers = [30, 40, 4, 5, 30, 50, 45];
 
-function getSecondLargest(arrayOfNumbers) {
-    let max = 0;
-    let secondMax = 0;
+// function getSecondLargest(arrayOfNumbers) {
+//     let max = 0;
+//     let secondMax = 0;
 
-    for (let index = 0; index < arrayOfNumbers.length; index++) {
-        const element = arrayOfNumbers[index];
+//     for (let index = 0; index < arrayOfNumbers.length; index++) {
+//         const element = arrayOfNumbers[index];
         
-        if (element > max) {
-            secondMax = max;
-            max = element;
-        } else if (element > secondMax && element !== max) {
-            secondMax = element;
-        }
-    }
-    return secondMax;
-}
+//         if (element > max) {
+//             secondMax = max;
+//             max = element;
+//         } else if (element > secondMax && element !== max) {
+//             secondMax = element;
+//         }
+//     }
+//     return secondMax;
+// }
 
-let result = getSecondLargest(numbers);
-console.log(result); 
+// let result = getSecondLargest(numbers);
+// console.log(result); 
+
+
+let test = [1,2,3,4,5,6,7,8,9,0];
+let len = test.length;
+console.log(len);
+
+console.log(pop(2));
